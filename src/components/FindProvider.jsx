0c6f5a27a3197c28310de4fe7f238f5780c903b7
Lutiago <perillaluisa11@gmail.com>
2023-04-06 00:32:17 +0000

    finalproject

--- src/components/FindProvider.jsx
+++ src/components/FindProvider.jsx
@@ -1,77 +1,44 @@
 import { useState } from "react";
 import { Button, InputGroup, Dropdown, DropdownButton } from "react-bootstrap";
-export const  FindProvider = () => {
-          const [selectedItem, setSelectedItem] = useState("Action");
-        
-          const handleItemClick = (item) => {
-            setSelectedItem(item);
-          };
-        
-          return (
-            <>
-              <InputGroup className="mb-3">
-                <Button variant="outline-secondary">What type of service?</Button>
-                <DropdownButton
-                  variant="outline-secondary"
-                  title=""
-                  id="input-group-dropdown-1"
-                >
-                  <Dropdown.Item onClick={() => handleItemClick("Action")}>
-                    Individual therapy
-                  </Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleItemClick("Another action")}>
-                    Couples therapy
-                  </Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleItemClick("Something else here")}>
-                    Family therapy
-                  </Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleItemClick("Something else here")}>
-                    Child and adolescent therapy
-                  </Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleItemClick("Something else here")}>
-                    Medication therapy
-                  </Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item onClick={() => handleItemClick("Separated link")}>
-                    Yoga
-                  </Dropdown.Item>
-                </DropdownButton>
-                <input
-                  type="text"
-                  className="form-control"
-                  aria-label="Text input with segmented dropdown button"
-                />
-              </InputGroup>
-        
-              <InputGroup>
-                <input
-                  type="text"
-                  className="form-control"
-                  aria-label="Text input with segmented dropdown button"
-                />
-                <Button variant="outline-secondary">Action</Button>
-                <DropdownButton
-                  variant="outline-secondary"
-                  title="Toggle Dropdown"
-                  id="input-group-dropdown-2"
-                  alignRight
-                >
-                  <Dropdown.Item onClick={() => handleItemClick("Action")}>
-                    Action
-                  </Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleItemClick("Another action")}>
-                    Another action
-                  </Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleItemClick("Something else here")}>
-                    Something else here
-                  </Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item onClick={() => handleItemClick("Separated link")}>
-                    Separated link
-                  </Dropdown.Item>
-                </DropdownButton>
-              </InputGroup>
-            </>
-          );
-        }
-  
+import { MdChair } from "react-icons/md";
+import { BsFillHouseHeartFill } from "react-icons/bs";
+
+export const FindProvider = () => {
+  const [selectedItem, setSelectedItem] = useState("Action");
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  return (
+    <>
+      <div className="input-group mb-3">
+        <label className="input-group-text" for="inputGroupSelect01">
+          <MdChair /> What type of service?
+        </label>
+        <select className="form-select" id="inputGroupSelect01">
+          <option selected>Choose...</option>
+          <option value="1">Individual therapy</option>
+          <option value="2">Couples therapy</option>
+          <option value="3">Family therapy</option>
+          <option value="4">Child and adolescent therapy</option>
+          <option value="5">Medication therapy</option>
+        </select>
+      </div>
+
+      <div className="input-group mb-3">
+        <label className="input-group-text" for="inputGroupSelect01">
+          <BsFillHouseHeartFill /> What type of service?
+        </label>
+        <select className="form-select" id="inputGroupSelect01">
+          <option selected>Choose...</option>
+          <option value="1">Individual therapy</option>
+          <option value="2">Couples therapy</option>
+          <option value="3">Family therapy</option>
+          <option value="4">Child and adolescent therapy</option>
+          <option value="5">Medication therapy</option>
+        </select>
+      </div>
+    </>
+  );
+};
--- src/components/MainSlide.jsx
+++ src/components/MainSlide.jsx
@@ -1,30 +1,36 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function MainSlide() {
   return (
     <div className="relative d-flex flex-row justify-content-center">
       {" "}
       <div className="relative">
-      <Image
-        src="/images/Rectangle 1.png"
-        className=" grid text-center"
-        width={800}
-        height={500}
-        />
-       <div className="position-absolute top-50 start-50 translate-middle">
-        <div className="d-flex align-items-center justify-content-center text-white">
-          <span className="fs-2"> Mental health care<br/> that make sense</span>
-        </div>
-        <button type="button" className="btn btn-outline-dark mt-3">FIND THERAPIST</button>
-        </div>
-        </div>
-    
         <Image
-          src="/images/homepage2.png"
+          src="/images/Rectangle 1.png"
           className=" grid text-center"
-          width={500}
+          width={800}
           height={500}
         />
+        <div className="position-absolute top-50 start-50 translate-middle">
+          <div className="d-flex align-items-center justify-content-center text-white">
+            <span className="fs-2">
+              {" "}
+              Mental health care
+              <br /> that make sense
+            </span>
+          </div>
+          <Link className="btn btn-outline-dark mt-3" href="/FindTherapist">
+            FIND THERAPIST
+          </Link>
+        </div>
       </div>
+      <Image
+        src="/images/homepage2.png"
+        className=" grid text-center"
+        width={500}
+        height={500}
+      />
+    </div>
   );
 }
