30160978dceb9808782c79970d8060a10a6fa7bf
Lutiago <perillaluisa11@gmail.com>
2023-04-10 20:29:44 +0000

    finalproject

---
+++ src/components/ProviderBottom.jsx
@@ -0,0 +1,27 @@
+export const ProviderBottom = () => {
+    return (
+
+<div className="d-flex align-items-left justify-content-left">
+        <span className="fs-2">
+          How does
+          <br /> C Healing work
+        </span>
+      </div>
+
+      <div className="d-flex align-items-center justify-content-right">
+     <span className="fs-7">
+          C Healing is a community of independent mental health care providers.
+          While our team helps you navigate the search process, we don’t
+          prescribe the rest. Each provider at C Healing sets their own rates
+          and makes their own decisions about going in-network with major
+          insurance payers.
+
+           <p>1. Browse our directory 
+           2. Book a consultation 
+           3. Decide on the right fit for you</p>
+
+           
+     </span>
+      </div>
+    );
+}
