e21fa1988b50e5ba986680672376c1a4162f4866
Lutiago <perillaluisa11@gmail.com>
2023-04-27 01:02:06 +0000

    finalproject

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -1,13 +1,24 @@
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 export const Contactform = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    
-      <div className="container form"> <h1>Book a Consultation</h1>
+    <div className="container form">
+      {" "}
+      <h1>Book a Consultation</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="row g-3 justify-content-md-center mb-3">
           <div className="col-md-6">
-           
             <input
+              {...register("full_name")}
               type="text"
               className="form-control"
               placeholder="Full Name"
@@ -16,16 +27,18 @@ export const Contactform = () => {
           </div>
           <div className="col-md-4">
             <input
+              {...register("email")}
               type="text"
               className="form-control"
               placeholder="Email"
               aria-label="Email"
             />
           </div>
-          </div>
-          <div className="row justify-content-md-center mb-3">
+        </div>
+        <div className="row justify-content-md-center mb-3">
           <div className="col-md-4">
             <input
+              {...register("phone")}
               type="text"
               className="form-control"
               placeholder="Phone"
@@ -33,95 +46,131 @@ export const Contactform = () => {
             />
           </div>
 
-          <div className="form check col">
-            <input className="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"/>
-              <label className="form-check-label" htmlFor="flexCheckDefault">
-                Monday
-              </label>
-            <input className="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"/>
-              <label className="form-check-label" htmlFor="flexCheckDefault">
-                Tuesday
-              </label>
-            <input className="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"/>
-              <label className="form-check-label" htmlFor="flexCheckDefault">
-                Wednesday
-              </label>
-            <input className="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"/>
-              <label className="form-check-label" htmlFor="flexCheckDefault">
-                Thursday
-              </label>
-            <input className="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"/>
-              <label className="form-check-label" htmlFor="flexCheckDefault">
-                Friday
-              </label>
-              
-          <div className="container"> 
-          <input className="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"/>
+          <div className="form check col ">
+            <input
+              {...register("day_monday")}
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Monday
+            </label>
+            <input
+              {...register("day_tuesday")}
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Tuesday
+            </label>
+            <input
+              {...register("day_wednesday")}
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Wednesday
+            </label>
+            <input
+              {...register("day_thursday")}
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Thursday
+            </label>
+            <input
+              {...register("day_friday")}
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Friday
+            </label>
+
+            <div className="container">
+              <input
+                {...register("time_morning")}
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+              />
               <label className="form-check-label" htmlFor="flexCheckDefault">
                 Morning
               </label>
-          <input className="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"/>
+              <input
+                {...register("time_afternoon")}
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+              />
               <label className="form-check-label" htmlFor="flexCheckDefault">
                 Afternoon
               </label>
-          <input className="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"/>
+              <input
+                {...register("time_night")}
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+              />
               <label className="form-check-label" htmlFor="flexCheckDefault">
                 Night
               </label>
-              
+            </div>
+
+            <div className="row g-3 justify-content-md">
+              <div className="col-md-4">
+                <textarea
+                  rows={7}
+                  className="form-control"
+                  placeholder="What brings you here today?"
+                  aria-label=""
+                />
               </div>
 
-             
-          <div className="row g-3 justify-content-md-center mb-3">
-          <div className="col-md-4">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="What brings you here today?"
-              aria-label=""
-            />
+              <div className="container">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="flexCheckDefault"
+                />{" "}
+                <label className="form-check-label" htmlFor="flexCheckDefault">
+                  virtual
+                </label>
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="flexCheckDefault"
+                />
+                <label className="form-check-label" htmlFor="flexCheckDefault">
+                  In Person
+                </label>
+              </div>
+            </div>
           </div>
-          <div className="col-md-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Virtual / In person"
-              aria-label=""
-            />
-          </div>
-</div>
 
+          <p>Our provider will reach out to you to schedule the consultation</p>
+          <div className="btn btn-outline-secondary">
+            <Link href="/">Send</Link>
           </div>
-          
-          <p>
-            Our provider will reach out to you to schedule the consultation
-          </p>
-          <div className="btn btn-outline-secondary"><Link href="/">Send</Link></div>
         </div>
-</div>
+        <input type="submit" />
+      </form>
+    </div>
   );
 };
