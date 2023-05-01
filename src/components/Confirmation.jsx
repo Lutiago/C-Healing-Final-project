be4d23e1d3b06ae00ead657e5964275f0d2e695f
Lutiago <perillaluisa11@gmail.com>
2023-05-01 19:13:51 +0000

    finalproject

---
+++ src/components/Confirmation.jsx
@@ -0,0 +1,20 @@
+
+
+export const Confirmation = () => {
+    return (
+        <div className="container"> <h1>That's all!</h1>
+        <div className="row p-2 justify-content-md-center">
+          <div className="">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title"></h5>
+                <p className="card-text">
+                The provider will contacting you soon.
+                <br />Thank you for your registration!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div></div>
+        );
+    }
---
+++ src/pages/confirmation.jsx
@@ -0,0 +1,9 @@
+import { Confirmation } from "@/components/Confirmation";
+import { Contactform } from "@/components/Contactform";
+import { FindProvider } from "@/components/FindProvider";
+
+export default function ContactProvider() {
+  return <div className="text-center">
+    <Confirmation />
+  </div>;
+}
